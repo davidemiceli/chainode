842c2e30b03852ced2a604f757dcc0f85c3a3ff9
Davide Miceli <dauwak@gmail.com>
2018-12-29 10:57:08 +0100

    Remove old files and folders.

--- app/routes/auth/logout.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// Requirements
-const configs = require('../../configs/configs');
-
-module.exports = function(req) {
-  return true;
-};
--- app/routes/status/health.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-// Monitoring server health status
-module.exports = function(req, res, next) {
-  return res.json({status: 'active'});
-}
